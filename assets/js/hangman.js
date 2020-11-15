f0808ace5d2d93805df2fc272e10d6f3ef8d0e85
Daisy McGirr <mcgirr.daisy@gmail.com>
2020-11-15 10:59:39 +0000

    Added arrays that will be used to pick words and hints for the game

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -13,3 +13,17 @@ let selectedWord;
 
 // Get all elements with a class of figure-part and store to array
 const figureParts = document.querySelectorAll('.figure-part');
+
+// Array of possible words for the game
+const words = ['alligator', 'horse', 'giraffe', 'monkey', 'elephant', 'flamingo', 'mcdonalds', 'beyonce', 'kanye', 'eminem', 'prince',
+    'adele', 'sting', 'apple', 'tomato', 'lasagne', 'onions', 'gherkin', 'pizza', 'salmon', 'branzino', 'anchovies', 'stingray', 'castanets',
+    'cowbell', 'maraca', 'tambourine', 'xylophone', 'buckaroo', 'scrabble', 'excited', 'jealousy', 'purple', 'violet', 'Diamond', 'Emerald',
+    'Amethyst', 'Quartz'
+];
+
+// Array of hints to pair with counter-part words
+const hints = ['Animal', 'Animal', 'Animal', 'Animal', 'Animal', 'Animal', 'Fast Food', 'Musician', 'Musician', 'Musician', 'Musician',
+    'Musician', 'Musician', 'Food', 'Food', 'Food', 'Food', 'Food', 'Food', 'Fish', 'Fish', 'Fish', 'Fish', 'Musical Instrument', 'Musical Instrument',
+    'Musical Instrument', 'Musical Instrument', 'Musical Instrument', 'Game', 'Game', 'Emotion', 'Emotion', 'Colour', 'Colour', 'Precious Gem',
+    'Precious Gem', 'Precious Gem', 'Precious Gem',
+];
